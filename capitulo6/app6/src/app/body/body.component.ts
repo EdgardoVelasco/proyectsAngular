0200b0dba92b2a20976d1614fb9da367a7bcbc94
edgardo.nvf <edgardo.nvf@gmail.com>
2023-11-13 18:20:07 +0000

    cambios

--- capitulo6/app6/src/app/body/body.component.ts
+++ capitulo6/app6/src/app/body/body.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
   data:string="";
+  vocales:string[]=[];
+  noVocales:string[]=[];
 
   onChangeValue(value:string){
     if(value.toUpperCase().match(/^[A-Z]{3,}/)){
@@ -18,5 +20,26 @@ export class BodyComponent {
   condicion():boolean{
     return this.data.match(/^[A,E,I,O,U].+/)!==null;
   }
+
+  private buscar(lista:string[]):string{
+    for(let tmp of lista){
+      if(tmp.toUpperCase()===this.data){
+           return tmp;
+      }
+    }
+    return "";
+  }
+
+  insertar(){
+    if(this.condicion() && this.buscar(this.vocales).length==0){
+      this.vocales.push(this.data);
+    }else{
+       if(this.data.length>1 && this.buscar(this.noVocales).length==0){
+         this.noVocales.push(this.data);
+       }
+    }
+  }
+
+
 
 }
