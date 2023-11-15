4876a4832ffb68e464cba3c0e52342672b27e640
edgardo.nvf <edgardo.nvf@gmail.com>
2023-11-15 20:13:00 +0000

    data

--- capitulo11/app11/src/app/app.component.ts
+++ capitulo11/app11/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app11';
+
+  formulario1= new FormGroup({
+    nombre:new FormControl("", [Validators.required]),
+    edad: new FormControl(""),
+    correo: new FormControl("", [Validators.required])
+  });
+
+  onSubmit(){
+    if(!this.nameErrors && !this.correoErrors){
+     console.log("-*-*-*Valores de formulario-*-*-*");
+     console.log(`nombre: ${this.formulario1.value.nombre??"default"}`);
+     console.log(`edad: ${this.formulario1.value.edad??0}`);
+     console.log(`correo: ${this.formulario1.value.correo??"default"}`);
+    }
+  }
+
+  get nameErrors():boolean{
+    return this.formulario1.get("nombre")?.errors?.['required']??false;
+  }
+
+  get correoErrors():boolean{
+    return this.formulario1.get("correo")?.errors?.['required']??false;
+  }
+
+
 }
