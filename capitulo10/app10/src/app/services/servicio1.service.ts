eb0ce628a29f46d9ea495f69cd6e0f62da1c980d
edgardo.nvf <edgardo.nvf@gmail.com>
2023-11-15 17:12:40 +0000

    data

--- capitulo10/app10/src/app/services/servicio1.service.ts
+++ capitulo10/app10/src/app/services/servicio1.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,16 @@ export class Servicio1Service {
   private _titulo:string="mi pagina web";
   private _nombre:string="edgardo";
   private _apellido:string="velasco";
-  constructor() { }
+  private _fechaObservable:Observable<Date>;
+  
+  constructor() { 
+    this._fechaObservable=new Observable(
+      observer=>{
+        setInterval(()=>observer.next(new Date()), 1000);
+      }
+    );
+
+  }
 
   get titulo():string{return this._titulo;}
   get nombre():string{return this._nombre;}
